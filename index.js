1cbbe7f4309e9ad4b733eaa671a5e6a030451a26
・ω・（竹子） <you948712@gmail.com>
2026-02-13 08:41:13 +0800

    chore: change winston to pino

--- index.js
+++ index.js
@@ -55,18 +55,45 @@ class Plugin {
 
     if(TREM.variable.speech) this.changeVoice(TREM.variable.speech)
 
-    const winstonLogger = logger._getLogger();
+    // 獲取 Pino 實例
+    const pinoLogger = logger._getLogger();
 
-    winstonLogger.transports.forEach(transport => {
-      const originalLog = transport.log.bind(transport);
-      transport.log = (info, callback) => {
+    /**
+     * PINO 修改部分：
+     * Pino 不像 Winston 有 transports 陣列可以遍歷。
+     * 我們需要攔截觸發 "Speech ready!" 的日誌方法（通常是 info）。
+     */
 
-        if (info.message == "Speech ready!") {
-          this.changeVoice(TREM.variable.speech)
-        }
-        return originalLog(info, callback);
-      };
-    });
+    // 1. 綁定原始的 info 方法以保留上下文
+    const originalInfo = pinoLogger.info.bind(pinoLogger);
+
+    // 2. 覆寫 info 方法
+    pinoLogger.info = (...args) => {
+      // Pino 的參數形式通常是: (msg), (obj, msg), 或 (obj)
+      // 我們需要檢查參數中是否包含目標訊息
+      let isTargetMessage = false;
+
+      // 檢查第一個參數是字串的情況: logger.info("Speech ready!")
+      if (typeof args[0] === 'string' && args[0] === "Speech ready!") {
+        isTargetMessage = true;
+      }
+      // 檢查第二個參數是字串的情況: logger.info({context}, "Speech ready!")
+      else if (typeof args[1] === 'string' && args[1] === "Speech ready!") {
+        isTargetMessage = true;
+      }
+      // 檢查物件中包含 msg 屬性的情況: logger.info({ msg: "Speech ready!" })
+      else if (args[0] && typeof args[0] === 'object' && args[0].msg === "Speech ready!") {
+        isTargetMessage = true;
+      }
+
+      // 如果檢測到目標訊息
+      if (isTargetMessage) {
+        this.changeVoice(TREM.variable.speech);
+      }
+
+      // 3. 執行原始的日誌記錄，確保日誌仍被輸出
+      return originalInfo(...args);
+    };
   }
 
   changeVoice(speech) {
